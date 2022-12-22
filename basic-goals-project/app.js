aee221610a42ff86dacc37f71a0a06f62d9b8eca
Gagan <g.deepsingh1@gmail.com>
2022-12-22 12:11:45 -0700

    methods in vue app

--- basic-goals-project/app.js
+++ basic-goals-project/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: "Finish the course and learn vue.js",
             vueLink: "https://vuejs.org"
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return "Learn vue";
+            } else {
+                return "Master vue";
+            }
+        }
     }
 });
 
