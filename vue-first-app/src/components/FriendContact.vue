b78d52f548d7c09f85387d2e14ce13bc3974e7cf
Gagan <g.deepsingh1@gmail.com>
2022-12-29 11:25:45 -0700

    adding component app

---
+++ vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggle">Show details</button>
+        <ul v-if="isdetailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li> 
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isdetailsAreVisible: false,
+                friend: {
+                    id: "manuel",
+                    name: "Manuel Lorenz",
+                    phone: "01243 45678 90",
+                    email: "manuel@localhost.com"
+                }   
+            }
+        },
+        methods: {
+            toggle() {
+                this.isdetailsAreVisible = !this.isdetailsAreVisible;
+            }
+        }
+    };
+</script>
