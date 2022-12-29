749e8d5f53a2e8121565e8c89abdb59b24ff8cb9
Gagan <g.deepsingh1@gmail.com>
2022-12-29 11:54:19 -0700

    pascal cases app

--- vue-first-app/src/components/FriendContact.vue
+++ vue-first-app/src/components/FriendContact.vue
@@ -4,14 +4,14 @@
         <button @click="toggle">{{ isdetailAreVisible ? 'Hide' : 'Show' }} details</button>
         <ul v-if="isdetailAreVisible">
             <li><strong>Phone:</strong>{{ phone }}</li> 
-            <li><strong>Email:</strong>{{ email }}</li>
+            <li><strong>Email:</strong>{{ emailAddress }}</li>
         </ul>
     </li>
 </template>
 
 <script>
     export default {
-        props: ['name', 'phone', 'email'],
+        props: ['name', 'phone', 'emailAddress'],
         data() {
             return {
                 isdetailAreVisible: false,
@@ -19,7 +19,6 @@
         },
         methods: {
             toggle() {
-                console.log(this.name);
                 this.isdetailAreVisible = !this.isdetailAreVisible;
             }
         }
