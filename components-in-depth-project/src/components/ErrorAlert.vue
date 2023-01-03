cd0f1f2be67d61d65b8454f2db7ce4f222c3cda0
Gagan <g.deepsingh1@gmail.com>
2023-01-03 13:10:15 -0700

    alert on error app

---
+++ components-in-depth-project/src/components/ErrorAlert.vue
@@ -0,0 +1,18 @@
+<template>
+    <dialog open>
+        <slot></slot>
+    </dialog>
+</template>
+
+<style scoped>
+    dialog {
+        margin: 0;
+        position: fixed;
+        top: 20vh;
+        left: 30%;
+        width: 40%;
+        box-shadow: 0 2px 6px 0px rgba(0,0,0,9.0);
+        background-color: white;
+        padding: 1rem;
+    }
+</style>
--- components-in-depth-project/src/components/ManageGoals.vue
+++ components-in-depth-project/src/components/ManageGoals.vue
@@ -1,4 +1,35 @@
 <template>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="isInvalidGoal">
+        <h2>Invalid goal</h2>
+        <p>Please enter a valid value</p>
+        <button @click="close">Ok</button>
+    </error-alert>
 </template>
+
+<script>
+    import ErrorAlert from "./ErrorAlert.vue";
+
+    export default {
+        components: {
+            ErrorAlert
+        },
+        data() {
+            return {
+                isInvalidGoal: false,
+            }
+        },
+        methods: {
+            setGoal() {
+                if (this.$refs.goal.value === "") {
+                    this.isInvalidGoal = true;
+                }
+            },
+            close() {
+                this.isInvalidGoal = false;
+            }
+        }
+    }
+</script>
