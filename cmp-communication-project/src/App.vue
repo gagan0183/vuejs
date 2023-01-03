1d8712af046d09e4a0d0572ff1ea8006e6d1c8aa
Gagan <g.deepsingh1@gmail.com>
2023-01-02 18:00:45 -0700

    provide inject patterns app

--- cmp-communication-project/src/App.vue
+++ cmp-communication-project/src/App.vue
@@ -4,7 +4,10 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base
+      :topics="topics"
+      @select-topic="activateTopic"
+    ></knowledge-base>
   </div>
 </template>
 
@@ -30,6 +33,11 @@ export default {
         },
       ],
       activeTopic: null,
+    };
+  },
+  provide() {
+    return {
+      topics: this.topics,
     };
   },
   methods: {
