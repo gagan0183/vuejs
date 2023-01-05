08eff7c94cf067415b066c71aa33f55b52568976
Gagan <g.deepsingh1@gmail.com>
2023-01-05 11:32:01 -0700

    style, header and base card components app

--- component-in-actions-project/src/App.vue
+++ component-in-actions-project/src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <ul>
-    <resource-item
-      v-for="resource in storedResources"
-      :key="resource.id"
-      :title="resource.title"
-      :description="resource.description"
-      :link="resource.link"
-    ></resource-item>
-  </ul>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import ResourceItem from './components/learning-resources/ResourceItem.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
-    ResourceItem,
+    StoredResources,
+    TheHeader,
   },
   data() {
     return {
@@ -37,3 +32,19 @@ export default {
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
