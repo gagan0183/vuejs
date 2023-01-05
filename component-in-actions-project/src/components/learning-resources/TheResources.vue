e5e4427197e20ef960522d565b1013194dd274b0
Gagan <g.deepsingh1@gmail.com>
2023-01-05 11:53:55 -0700

    style add tab app

--- component-in-actions-project/src/components/learning-resources/TheResources.vue
+++ component-in-actions-project/src/components/learning-resources/TheResources.vue
@@ -1,9 +1,13 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')"
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('add-resource')"
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
       >Add Resource</base-button
     >
   </base-card>
@@ -19,9 +23,31 @@ export default {
     StoredResources,
     AddResource,
   },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
   data() {
     return {
       selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google',
+          link: 'https://google.com',
+        },
+      ],
     };
   },
   methods: {
@@ -29,5 +55,12 @@ export default {
       this.selectedTab = tab;
     },
   },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
 };
 </script>
+
+<style scoped></style>
