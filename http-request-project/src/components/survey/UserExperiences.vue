dca4183a0d5637f228ccb9fb333902ec2f912579
Gagan <g.deepsingh1@gmail.com>
2023-01-06 12:58:19 -0700

    getting information app

--- http-request-project/src/components/survey/UserExperiences.vue
+++ http-request-project/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperience"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,33 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperience() {
+      fetch(
+        'https://vue-http-project-492bd-default-rtdb.firebaseio.com/surveys.json'
+      ).then(async (response) => {
+        if (response.ok) {
+          const data = await response.json();
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        }
+      });
+    },
   },
 };
 </script>
