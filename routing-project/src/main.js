25e5371e3a88509f3fb39db828252dfd52949cba
Gagan <g.deepsingh1@gmail.com>
2023-01-11 22:41:04 -0700

    catch the route app

--- routing-project/src/main.js
+++ routing-project/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotValid from './components/nav/NotValid.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -26,6 +27,10 @@ const router = createRouter({
       path: '/teams/:teamId',
       component: TeamMembers,
       props: true,
+    },
+    {
+      path: '/:NotValid(.*)',
+      component: NotValid,
     },
   ],
 });
