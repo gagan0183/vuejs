de2d37b38c3205e7201faea11e2b70798674fb90
Gagan <g.deepsingh1@gmail.com>
2023-01-14 09:07:45 -0700

    transition vue app

--- animations-project/src/App.vue
+++ animations-project/src/App.vue
@@ -90,6 +90,35 @@ button:active {
   /* transform: translateX(-190px); */
   animation: slide-out 0.3s ease-out forwards;
 }
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(50px);
+}
+
 @keyframes slide-out {
   0% {
     transform: translateX(0) scale(1);
