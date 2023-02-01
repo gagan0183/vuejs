07d35a1d96e0e60f0b62d1684ddc9c6e289411e1
Gagan <g.deepsingh1@gmail.com>
2023-01-31 17:28:07 -0700

    state management

--- coach webapp/src/main.js
+++ coach webapp/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import Router from './router';
+
+import store from './store/index.js';
 import App from './App.vue';
 
-const app = createApp(App).use(Router);
+const app = createApp(App).use(Router).use(store);
 
 app.mount('#app');
