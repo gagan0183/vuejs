310b920e8a5048bc482638df99cb5f2790121c09
Gagan <g.deepsingh1@gmail.com>
2023-01-31 22:45:15 -0700

    coaches components changes

--- coach webapp/src/main.js
+++ coach webapp/src/main.js
@@ -3,7 +3,16 @@ import Router from './router';
 
 import store from './store/index.js';
 import App from './App.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
 
-const app = createApp(App).use(Router).use(store);
+const app = createApp(App);
+app.use(Router);
+app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
