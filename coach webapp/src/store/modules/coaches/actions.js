7194ee489f74f15f4793c97fc2bf228142e2c947
Gagan <g.deepsingh1@gmail.com>
2023-02-05 19:30:26 -0700

    save coaches app

--- coach webapp/src/store/modules/coaches/actions.js
+++ coach webapp/src/store/modules/coaches/actions.js
@@ -1,13 +1,29 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit('registerCoach', coachData);
+
+    const response = await fetch(
+      `https://vue-http-project-492bd-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+    // const responseData = response.json();
+    if (!response.ok) {
+      // error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
   },
 };
