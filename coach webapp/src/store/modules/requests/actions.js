95b0c59a1cf40df124c334c9dcae14677c118823
Gagan <g.deepsingh1@gmail.com>
2023-02-07 19:45:16 -0700

    save request changes

--- coach webapp/src/store/modules/requests/actions.js
+++ coach webapp/src/store/modules/requests/actions.js
@@ -1,11 +1,25 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `https://vue-http-project-492bd-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'post',
+        body: JSON.stringify(newRequest),
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(response.data.message || 'Failed to fecth!');
+      throw error;
+    }
+
+    newRequest.id = responseData.name;
+
     context.commit('addRequest', newRequest);
   },
 };
