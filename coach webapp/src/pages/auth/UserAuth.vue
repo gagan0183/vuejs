c9d37b335c205d1f1a2d805c5d0813a286c79bbf
Gagan <g.deepsingh1@gmail.com>
2023-02-13 23:09:08 -0700

    user auth template changes

---
+++ coach webapp/src/pages/auth/UserAuth.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <div class="form-control">
+      <label for="email">Email</label>
+      <input id="email" type="email" />
+    </div>
+    <div class="form-control">
+      <label for="password">Password</label>
+      <input id="password" type="password" />
+    </div>
+    <base-button>Login</base-button>
+  </form>
+</template>
